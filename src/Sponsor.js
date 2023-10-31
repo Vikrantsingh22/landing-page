d5a294b4579c3861d453d7b5df48c55b043fc23e
Alstudd <alstonsoares17@gmail.com>
2023-10-31 23:04:04 +0530

    added sponsor and prizes

--- src/Sponsor.js
+++ src/Sponsor.js
@@ -1,133 +1,149 @@
-import React from 'react'
+import React from "react";
 import "./Sponsor.css";
 
 function Sponsor() {
   return (
-    <div className='Sponsorbody'>
-        <div className="sponContainer">
-      <div className="card1">
-        <div className="card-inner">
-          <div className="front">
-            <div className="img-box"></div>
-          </div>
-          <div className="back">
-            <h1>Text 1</h1>
-            <p>These are our hackanova sponsors of hackaova3.0</p>
-            <div className="row">
-              <div className="col">
-                <h2>12k</h2>
-                <p>Followers</p>
-              </div>
-              <div className="col">
-                <h2>1k</h2>
-                <p>Following</p>
-              </div>
-              <div className="col">
-                <h2>13k</h2>
-                <p>Likes</p>
-              </div>
+    <div className="Sponsorbody">
+      <div className="sponContainer">
+        <div className="card1">
+          <div className="card-inner">
+            <div className="front">
+              <div className="img-box"></div>
             </div>
-            <div className="row">
-              <button>Follow</button>
-              <a href="#"><img src="instagram.png" /></a>
-              <a href="#"><img src="linkedin.png" /></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card2">
-        <div className="card-inner">
-          <div className="front">
-            <div className="img-box"></div>
-          </div>
-          <div className="back">
-            <h1>Text 1</h1>
-            <p>These are our hackanova sponsors of hackaova3.0</p>
-            <div className="row">
-              <div className="col">
-                <h2>4k</h2>
-                <p>Following</p>
+            <div className="back">
+              <h1>Text 1</h1>
+              <p>These are our hackanova sponsors of hackaova3.0</p>
+              <div className="row">
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Followers</p>
+                </div>
+                <div className="col">
+                  <h2>1k</h2>
+                  <p>Following</p>
+                </div>
+                <div className="col">
+                  <h2>13k</h2>
+                  <p>Likes</p>
+                </div>
               </div>
-              <div className="col">
-                <h2>12k</h2>
-                <p>Followers</p>
+              <div className="row">
+                <button>Follow</button>
+                <a href="https://www.google.com">
+                  <img src="/instagram.png" alt="" />
+                </a>
+                <a href="https://www.google.com">
+                  <img src="/linkedin.png" alt="" />
+                </a>
               </div>
-              <div className="col">
-                <h2>2k</h2>
-                <p>Likes</p>
-              </div>
-            </div>
-            <div className="row">
-              <button>Follow</button>
-              <a href="#"><img src="instagram.png" /></a>
-              <a href="#"><img src="linkedin.png" /></a>
             </div>
           </div>
         </div>
-      </div>
-      <div className="card3">
-        <div className="card-inner">
-          <div className="front">
-            <div className="img-box"></div>
-          </div>
-          <div className="back">
-            <h1>Text 1</h1>
-            <p>These are our hackanova sponsors of hackaova3.0</p>
-            <div className="row">
-              <div className="col">
-                <h2>12k</h2>
-                <p>Followers</p>
-              </div>
-              <div className="col">
-                <h2>12k</h2>
-                <p>Following</p>
-              </div>
-              <div className="col">
-                <h2>4k</h2>
-                <p>Likes</p>
-              </div>
+        <div className="card2">
+          <div className="card-inner">
+            <div className="front">
+              <div className="img-box"></div>
             </div>
-            <div className="row">
-              <button>Follow</button>
-              <a href="#"><img src="instagram.png" /></a>
-              <a href="#"><img src="linkedin.png" /></a>
+            <div className="back">
+              <h1>Text 1</h1>
+              <p>These are our hackanova sponsors of hackaova3.0</p>
+              <div className="row">
+                <div className="col">
+                  <h2>4k</h2>
+                  <p>Following</p>
+                </div>
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Followers</p>
+                </div>
+                <div className="col">
+                  <h2>2k</h2>
+                  <p>Likes</p>
+                </div>
+              </div>
+              <div className="row">
+                <button>Follow</button>
+                <a href="https://www.google.com">
+                  <img src="/instagram.png" alt="" />
+                </a>
+                <a href="https://www.google.com">
+                  <img src="/linkedin.png" alt="" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="card4">
-        <div className="card-inner">
-          <div className="front">
-            <div className="img-box"></div>
-          </div>
-          <div className="back">
-            <h1>Text 1</h1>
-            <p>These are our hackanova sponsors of hackaova3.0</p>
-            <div className="row">
-              <div className="col">
-                <h2>12k</h2>
-                <p>Followers</p>
+        <div className="card3">
+          <div className="card-inner">
+            <div className="front">
+              <div className="img-box"></div>
+            </div>
+            <div className="back">
+              <h1>Text 1</h1>
+              <p>These are our hackanova sponsors of hackaova3.0</p>
+              <div className="row">
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Followers</p>
+                </div>
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Following</p>
+                </div>
+                <div className="col">
+                  <h2>4k</h2>
+                  <p>Likes</p>
+                </div>
               </div>
-              <div className="col">
-                <h2>12k</h2>
-                <p>Following</p>
-              </div>
-              <div className="col">
-                <h2>5k</h2>
-                <p>Likes</p>
+              <div className="row">
+                <button>Follow</button>
+                <a href="https://www.google.com">
+                  <img src="/instagram.png" alt="" />
+                </a>
+                <a href="https://www.google.com">
+                  <img src="/linkedin.png" alt="" />
+                </a>
               </div>
             </div>
-            <div className="row">
-              <button>Follow</button>
-              <a href="./"><img src="instagram.png" alt="" /></a>
-              <a href="./"><img src="linkedin.png" alt="" /></a>
+          </div>
+        </div>
+        <div className="card4">
+          <div className="card-inner">
+            <div className="front">
+              <div className="img-box"></div>
+            </div>
+            <div className="back">
+              <h1>Text 1</h1>
+              <p>These are our hackanova sponsors of hackaova3.0</p>
+              <div className="row">
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Followers</p>
+                </div>
+                <div className="col">
+                  <h2>12k</h2>
+                  <p>Following</p>
+                </div>
+                <div className="col">
+                  <h2>5k</h2>
+                  <p>Likes</p>
+                </div>
+              </div>
+              <div className="row">
+                <button>Follow</button>
+                <a href="https://www.google.com">
+                  <img src="/instagram.png" alt="" />
+                </a>
+                <a href="https://www.google.com">
+                  <img src="/linkedin.png" alt="" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-        </div>
-  )
+  );
 }
 
-export default Sponsor
+export default Sponsor;
